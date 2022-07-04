819f459860a7152822434ae6389bbdf38cdd8373
Austin Hou <austin@austinhou.com>
2022-07-03 19:45:52 -0700

    image shadow fix

--- components/DetailedImageShowcase/DetailedImageShowcase.tsx
+++ components/DetailedImageShowcase/DetailedImageShowcase.tsx
@@ -25,13 +25,19 @@ const DetailedImageShowcase: React.FC<Props> = ({
     >
       <div
         className={cx(
-          "flex items-center px-2 w-full h-full shadow-xl rounded-image relative",
+          "flex items-center px-2 w-full h-full rounded-image relative",
           "lg:col-start-8 lg:col-end-13 lg:row-start-1",
           "md:col-start-7 md:col-end-13 md:row-start-1",
           "sm:col-start-6 sm:col-end-13 sm:row-start-1"
         )}
       >
-        <Image src={imageURL} alt="showcase image" width={800} height={450} />
+        <Image
+          src={imageURL}
+          alt="showcase image"
+          width={800}
+          height={450}
+          className="shadow-xl"
+        />
       </div>
       <div
         className={
