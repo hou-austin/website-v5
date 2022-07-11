b406d6874f10c6a502610cba2576e6dccbe8a605
kobespencer <kobephoward@gmail.com>
2022-07-11 12:44:00 -0700

    WIP multiimagedisplay init

---
+++ components/slices/MultiImageDisplay/MultiImageDisplay.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MultiImageDisplay from "graphql/schema.ts"
+
+type Props = {}
+
+const MultiImageDisplay = (props: Props) => {
+  return (
+    <div>MultiImageDisplay</div>
+  )
+}";
